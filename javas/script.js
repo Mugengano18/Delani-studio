2565d010c5621eafa1f1623ce81ec2aba4b17496
Mugengano18 <mugenganoa@gmail.com>
2019-07-25 17:37:38 +0200

    I added the jquery on the what we do part

--- javas/script.js
+++ javas/script.js
@@ -1,4 +1,14 @@
-// $(document).ready(function(){
-//     $(".header-showing").toggle();
-//     $(".par-hidden").toggle();
-// });
+$(document).ready(function(){
+    $(".click1").click(function(){
+        $("#par1").toggle();
+        $(".hidden1").toggle();
+    });
+    $(".click2").click(function(){
+        $("#par2").toggle();
+        $(".hidden2").toggle();
+    });
+    $(".click3").click(function(){
+        $("#par3").toggle();
+        $(".hidden3").toggle();
+    });
+});
